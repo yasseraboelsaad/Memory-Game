3cefc70653225c789ff44ff9b40ffeffb6819cd3
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2017-04-18 03:51:59 +0200

    added title and restart and info buttons

--- Memory Game/ViewController.swift
+++ Memory Game/ViewController.swift
@@ -17,15 +17,16 @@ class ViewController: UIViewController {
     var buttonsCount: Int = 0
     var Button : UIButton!
     
-    let array = ["aceOfHearts","JackOfSpades","QueenOfHearts","kingOfHearts","aceOfSpades","QueenOfSpades","JackOfHearts","kingOfSpades","JackOfHearts","aceOfSpades","QueenOfHearts","QueenOfSpades","kingOfHearts","kingOfSpades","aceOfHearts","JackOfSpades"]
+    let array = ["aceOfHearts","JackOfSpades","QueenOfHearts","kingOfHearts"
+                ,"aceOfSpades","QueenOfSpades","JackOfHearts","kingOfSpades"
+                ,"JackOfHearts","aceOfSpades","QueenOfHearts","QueenOfSpades"
+                ,"kingOfHearts","kingOfSpades","aceOfHearts","JackOfSpades"]
     @IBOutlet var buttonsArray: [UIButton]!
+    var gameCounter: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        for button in buttonsArray{
-            button.setTitle("⁇", for: .normal)
-        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,30 +36,71 @@ class ViewController: UIViewController {
 
     @IBAction func buttonClicked(_ sender: UIButton) {
         if buttonsCount==0 {
-            sender.setImage(UIImage(named: array[sender.tag]), for: .normal)
+            UIView.transition(with: sender, duration: 0.5, options: .transitionFlipFromRight, animations: {
+                sender.setImage(UIImage(named: self.array[sender.tag]), for: .normal)
+            }, completion: nil)
             firstButtonTag=sender.tag
             Button=sender
             firstButtonText=array[sender.tag]
             buttonsCount+=1
         }else if buttonsCount == 1{
-            sender.setImage(UIImage(named: array[sender.tag]), for: .normal)
+            UIView.transition(with: sender, duration: 0.5, options: .transitionFlipFromRight, animations: {
+                sender.setImage(UIImage(named: self.array[sender.tag]), for: .normal)
+            }, completion: nil)
             secondButtonTag=sender.tag
             secondButtonText=array[sender.tag]
-            let when = DispatchTime.now() + 2 // change 2 to desired number of seconds
+            let when = DispatchTime.now() + 1 // change 2 to desired number of seconds
             DispatchQueue.main.asyncAfter(deadline: when) {
                 // Your code with delay
                 if (self.firstButtonText == self.secondButtonText) && !(self.Button==sender) {
                     self.buttonsArray[self.firstButtonTag].isEnabled=false
                     self.buttonsArray[self.secondButtonTag].isEnabled=false
+                    self.gameCounter+=1
+                    if self.gameCounter==8{
+                        self.gameOver()
+                    }
                 }else{
-                    self.buttonsArray[self.firstButtonTag].setImage(#imageLiteral(resourceName: "back"), for: .normal)
-                    self.buttonsArray[self.secondButtonTag].setImage(#imageLiteral(resourceName: "back"), for: .normal)
+                    UIView.transition(with: self.buttonsArray[self.firstButtonTag], duration: 0.5, options: .transitionFlipFromLeft, animations: {
+                        self.buttonsArray[self.firstButtonTag].setImage(#imageLiteral(resourceName: "back"), for: .normal)
+                    }, completion: nil)
+                    UIView.transition(with: self.buttonsArray[self.secondButtonTag], duration: 0.5, options: .transitionFlipFromLeft, animations: {
+                        self.buttonsArray[self.secondButtonTag].setImage(#imageLiteral(resourceName: "back"), for: .normal)
+                    }, completion: nil)
                 }
             }
             
             buttonsCount=0
         }
         
+    }
+    @IBAction func restartGameButton() {
+        restartGame()
+    }
+    
+    @IBAction func info() {
+        let alert = UIAlertController(title: nil,message: "Click on the cards to flip them, try to flip two identical cards in a row to match them, may the odds be ever in your favour!", preferredStyle: .alert)
+        let action = UIAlertAction(title:"OK", style: .default,  handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true,completion: nil)
+    }
+    
+    func gameOver(){
+        let alert = UIAlertController(title: nil,message: "Well Done!", preferredStyle: .alert)
+        let action = UIAlertAction(title:"Play again!", style: .default,  handler: { action in
+            self.restartGame()
+        })
+        alert.addAction(action)
+        present(alert, animated: true,completion: nil)
+    }
+    
+    func restartGame(){
+        gameCounter=0
+        for button in buttonsArray{
+            UIView.transition(with: button, duration: 0.5, options: .transitionFlipFromLeft, animations: {
+                button.setImage(#imageLiteral(resourceName: "back"), for: .normal)
+            }, completion: nil)
+            button.isEnabled=true
+        }
     }
     
 
